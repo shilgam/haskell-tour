41184429a9a5607350e247d2798ee75443e82354
Ilgam Shakirov <shilgam@gmail.com>
2023-01-17 00:24:12 +0300

    Test method: isPalindrome

--- palindrome-testing/test/Spec.hs
+++ palindrome-testing/test/Spec.hs
@@ -1,2 +1,16 @@
+import Lib
+
+assert :: Bool -> String -> String -> IO ()
+assert test passStatement failStatement =
+    if test
+    then putStrLn passStatement
+    else putStrLn failStatement
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    putStrLn "Running tests..."
+    assert (isPalindrome "racecar") "passed: 'racecar'"
+                                    "failed: 'racecar'"
+    assert (isPalindrome "racecar!") "failed: 'racecar!'"
+                                     "passed: 'racecar!'"
